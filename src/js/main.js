c6534d3b4c75bcb147e88dca24059db9cc32b94a
Hada Jang <hada@Hadas-MacBook-Pro.local>
2025-01-13 21:28:09 -0800

    Add lottie animation for benefit section.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import '../styles/style.scss';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Accordion from './modules/Accordion';
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -10,3 +12,25 @@ new Accordion('#faq', {
   maxActive: 1
 });
 
+
+
+const benefitLottie1 = new DotLottie({
+  autoplay: true,
+  loop: true,
+  canvas: document.querySelector('#benefit1'),
+  src: "/lottie/simplify.lottie",
+});
+
+const benefitLottie2 = new DotLottie({
+  autoplay: true,
+  loop: true,
+  canvas: document.querySelector('#benefit2'),
+  src: "/lottie/timesave.lottie", 
+});
+
+const benefitLottie3 = new DotLottie({
+  autoplay: true,
+  loop: true,
+  canvas: document.querySelector('#benefit3'),
+  src: "/lottie/track.lottie",
+});
